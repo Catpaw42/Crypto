bb95366e40d63f870edf5c8822a96fd807f2d350
Cat <Cat@Cat-PC>
2014-06-23 00:12:33 +0200

    finished assignment 2

--- CryptoAnalysis/src/MD5Redux.java
+++ CryptoAnalysis/src/MD5Redux.java
@@ -1,45 +1,156 @@
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class MD5Redux
 {
-	public static String getMD5Hash(String text, int bitLimit)
+	public static int getMD5Hash(int hexInput, int bitSizeLimiter)
 	{
-		MessageDigest md;
-		
-		//TODO: check that length matches criteria if not, throw error
-		
 		try
 		{
-			md = MessageDigest.getInstance("MD5");
-			md.update(text.getBytes("UTF-8"));
+			//calculate the limit to use as a bit-mask
+			int mask = (int) (Math.pow(16, (bitSizeLimiter / 4))-1);
+			//use the mask to set any bits above the wanted limit to 0.
+			hexInput = mask & hexInput;
 			
+			//get the byte value 			   4 bytes for a 32-bit integer
+			byte[] bytes = ByteBuffer.allocate(4).putInt(hexInput).array();
+			
+			//create a MD5 digest
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(bytes);
 			byte[] digest = md.digest();
+
+			//get the integer value
+			int result = ByteBuffer.wrap(digest).getInt();
 			
-			BigInteger bigInt = new BigInteger(1,digest);
-			String result = bigInt.toString(16); //md5 produces hex so use radix 16
-			// add leading zeroes if below length limit
-			while(result.length() < 32 ) //TODO: change to length variable
-			  result = "0"+result;
+			//use the mask again to limit the output
+			result = mask & result;
 			return result;
 		}
-		catch (NoSuchAlgorithmException e)
+		catch (Exception e)
 		{
+			System.err.println(e.getMessage());
 			e.printStackTrace();
 		}
-		catch (UnsupportedEncodingException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return null;
+		return 0;
 	}
 	
+	public static int getMD5Hash(int hexInput, int i, int bitSizeLimiter)
+	{
+		int temp = getMD5Hash(hexInput, bitSizeLimiter);
+		temp = temp ^ i;
+		return temp;
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println(getMD5Hash("1", 20));
+		assignment17();
+	}
+	
+	private static void assignment17()
+	{
+		Random rand = new Random();
+		
+		int loopCount = (int)Math.pow(2, 16);	//how many chains
+		int chainLength = (int)Math.pow(2, 8);	//length of chain
+		int keySpace = (int)Math.pow(2, 20);	//key space
+		
+		//for now I'm storing start / end in arraylists.
+		ArrayList<Integer> startPoints = new ArrayList<Integer>();
+		ArrayList<Integer> endpoints = new ArrayList<Integer>();
+		
+		//to get random without repetition, I remove random elements from an ArrayList that covers keyspace
+		ArrayList<Integer> keys = new ArrayList<Integer>();
+		for (int i = 0; i < keySpace; i++)
+			keys.add(i);
+		
+		//used to count the total number of nodes checked
+		boolean[] taken = new boolean[keySpace]; 
+		ArrayList<Double> checked = new ArrayList<Double>();
+		
+		for (int i = 0; i < loopCount; i++)
+		{
+			//pick a random number
+			int pick = keys.remove(rand.nextInt(keys.size()));
+			startPoints.add(pick);
+			//run a chain, no checks for repetition
+			for (int j = 0; j < chainLength; j++)
+			{
+				pick = getMD5Hash(pick, j, 20);
+				taken[pick] = true;
+			}
+			endpoints.add(pick);
+			
+			//take samples every 100 chains
+			if(i % 100 == 0)
+			{
+				int count = 0;
+				for (int k = 0; k < taken.length; k++)
+					if(taken[k])
+						count++;
+				
+				checked.add((double)count / (double)taken.length);
+			}
+		}
+		NumberFormat formatter = new DecimalFormat("#0.000");
+		for (int i = 0; i < checked.size(); i++)
+			System.out.println(formatter.format(checked.get(i)* 100) + "%");
+	}
+	
+
+	private static void assignment15()
+	{
+		Random rand = new Random();
+		
+		int loopCount = (int)Math.pow(2, 16);	//how many chains
+		int chainLength = (int)Math.pow(2, 8);	//length of chain
+		int keySpace = (int)Math.pow(2, 20);	//key space
+		
+		//for now I'm storing start / end in arraylists.
+		ArrayList<Integer> startPoints = new ArrayList<Integer>();
+		ArrayList<Integer> endpoints = new ArrayList<Integer>();
+		
+		//to get random without repetition, I remove random elements from an ArrayList that covers keyspace
+		ArrayList<Integer> keys = new ArrayList<Integer>();
+		for (int i = 0; i < keySpace; i++)
+			keys.add(i);
+		
+		//used to count the total number of nodes checked
+		boolean[] taken = new boolean[keySpace]; 
+		ArrayList<Double> checked = new ArrayList<Double>();
+		
+		for (int i = 0; i < loopCount; i++)
+		{
+			//pick a random number
+			int pick = keys.remove(rand.nextInt(keys.size()));
+			startPoints.add(pick);
+			//run a chain, no checks for repetition
+			for (int j = 0; j < chainLength; j++)
+			{
+				pick = getMD5Hash(pick, 20);
+				taken[pick] = true;
+			}
+			endpoints.add(pick);
+			
+			//take samples every 100 chains
+			if(i % 100 == 0)
+			{
+				int count = 0;
+				for (int k = 0; k < taken.length; k++)
+					if(taken[k])
+						count++;
+				
+				checked.add((double)count / (double)taken.length);
+			}
+		}
+		
+		NumberFormat formatter = new DecimalFormat("#0.000");
+		for (int i = 0; i < checked.size(); i++)
+			System.out.println(formatter.format(checked.get(i)* 100) + "%");
 	}
 }
--- CryptoAnalysis/src/project2/EncFunc.java
+++ CryptoAnalysis/src/project2/EncFunc.java
@@ -1,6 +1,49 @@
 package project2;
 
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class EncFunc
 {
+	//the 28 bit hex challenge
+	private static int challenge = 0x0A85AFC;
+	
+	public static int function(int s)
+	{	
+		byte[] sBytes = ByteBuffer.allocate(4).putInt(s).array();
+		byte[] chalengeBytes = ByteBuffer.allocate(4).putInt(challenge).array();
 
+		byte[] total = new byte[sBytes.length + chalengeBytes.length];
+		
+		for (int i = 0; i < sBytes.length; i++)
+			total[i] = sBytes[i];
+		for (int i = sBytes.length; i < total.length; i++)
+			total[i] = chalengeBytes[i - sBytes.length];
+		
+		
+		MessageDigest md;
+		try
+		{
+			md = MessageDigest.getInstance("MD5");
+			
+			md.update(total);
+			byte[] digest = md.digest();
+
+			return ByteBuffer.wrap(digest).getInt();
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(Integer.toHexString(function(0x0AEEAFC)));
+		System.out.println(Integer.toHexString(function(0x1A85AFC)));
+		System.out.println(Integer.toHexString(function(0x0225AFC)));
+	}
 }
